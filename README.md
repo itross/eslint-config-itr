af7f88ee29209831b03c0f11e2b9ee7e8ff58825
joshuagame <joshuagame@gmail.com>
2023-07-24 11:35:51 +0200

    first commit

--- README.md
+++ README.md
@@ -2,6 +2,12 @@
 
 JavaScript base Style Guide for @itross projects.
 
+
+```
+> **Note**: this Style Guide is heavily based on the one from the [Node.js project](https://github.com/nodejs/node/blob/main/.eslintrc.js).
+```
+
+
 ## License
 
 [MIT](LICENSE)
---
+++ index.js
@@ -0,0 +1,151 @@
+module.exports = {
+  'env': {
+    'commonjs': true,
+    'es2021': true,
+    'node': true
+  },
+  'extends': 'eslint:recommended',
+  'overrides': [
+    {
+      'env': {
+          'node': true
+      },
+      'files': [
+          '.eslintrc.{js,cjs}'
+      ],
+      'parserOptions': {
+          'sourceType': 'script'
+      }
+    }
+  ],
+  'parserOptions': {
+    'ecmaVersion': 'latest'
+  },
+  'rules': {
+    'accessor-pairs': 'error',
+    'array-callback-return': 'error',
+    'arrow-parens': 'error',
+    'arrow-spacing': 'error',
+    'block-scoped-var': 'error',
+    'block-spacing': 'error',
+    'brace-style': ['error', '1tbs', { allowSingleLine: true }],
+    'capitalized-comments': ['error', 'always', {
+      line: {
+        // Ignore all lines that have less characters than 20 and all lines that
+        // start with something that looks like a variable name or code.
+        ignorePattern: '.{0,20}$|[a-z]+ ?[0-9A-Z_.(/=:[#-]|std|http|ssh|ftp',
+        ignoreInlineComments: true,
+        ignoreConsecutiveComments: true,
+      },
+      block: {
+        ignorePattern: '.*',
+      },
+    }],
+    'comma-dangle': ['error', 'always-multiline'],
+    'comma-spacing': 'error',
+    'comma-style': 'error',
+    'computed-property-spacing': 'error',
+    'default-case-last': 'error',
+    'dot-location': ['error', 'property'],
+    'dot-notation': 'error',
+    'eol-last': 'error',
+    'eqeqeq': ['error', 'smart'],
+    'func-call-spacing': 'error',
+    'func-name-matching': 'error',
+    'func-style': ['error', 'declaration', { allowArrowFunctions: true }],
+    'indent': ['error', 2, {
+      ArrayExpression: 'first',
+      CallExpression: { arguments: 'first' },
+      FunctionDeclaration: { parameters: 'first' },
+      FunctionExpression: { parameters: 'first' },
+      MemberExpression: 'off',
+      ObjectExpression: 'first',
+      SwitchCase: 1,
+    }],
+    'key-spacing': 'error',
+    'keyword-spacing': 'error',
+    'linebreak-style': 'error',
+    'max-len': ['error', {
+      code: 120,
+      ignorePattern: '^// Flags:',
+      ignoreRegExpLiterals: true,
+      ignoreTemplateLiterals: true,
+      ignoreUrls: true,
+      tabWidth: 2,
+    }],
+    'new-parens': 'error',
+    'no-confusing-arrow': 'error',
+    'no-constant-condition': ['error', { checkLoops: false }],
+    'no-constructor-return': 'error',
+    'no-duplicate-imports': 'error',
+    'no-else-return': 'error',
+    'no-extra-parens': ['error', 'functions'],
+    'no-lonely-if': 'error',
+    'no-mixed-requires': 'error',
+    'no-multi-spaces': ['error', { ignoreEOLComments: true }],
+    'no-multiple-empty-lines': ['error', { max: 2, maxEOF: 0, maxBOF: 0 }],
+    'no-new-require': 'error',
+    'no-path-concat': 'error',
+    'no-proto': 'error',
+    'no-redeclare': ['error', { 'builtinGlobals': false }],
+    'no-return-await': 'error',
+    'no-self-compare': 'error',
+    'no-tabs': 'error',
+    'no-template-curly-in-string': 'error',
+    'no-throw-literal': 'error',
+    'no-trailing-spaces': 'error',
+    'no-undef': ['error', { typeof: true }],
+    'no-undef-init': 'error',
+    'no-unused-expressions': ['error', { allowShortCircuit: true }],
+    'no-unused-vars': ['error', { args: 'none', caughtErrors: 'all' }],
+    'no-use-before-define': ['error', {
+      classes: true,
+      functions: false,
+      variables: false,
+    }],
+    'no-useless-call': 'error',
+    'no-useless-concat': 'error',
+    'no-useless-constructor': 'error',
+    'no-useless-return': 'error',
+    'no-var': 'error',
+    'no-void': 'error',
+    'no-whitespace-before-property': 'error',
+    'object-curly-newline': 'error',
+    'object-curly-spacing': ['error', 'always'],
+    'one-var': ['error', { initialized: 'never' }],
+    'one-var-declaration-per-line': 'error',
+    'operator-linebreak': ['error', 'after'],
+    'padding-line-between-statements': [
+      'error',
+      { blankLine: 'always', prev: 'function', next: 'function' },
+    ],
+    'prefer-const': ['error', { ignoreReadBeforeAssign: true }],
+    'prefer-object-has-own': 'error',
+    'quotes': ['error', 'single', { avoidEscape: true }],
+    'quote-props': ['error', 'consistent'],
+    'rest-spread-spacing': 'error',
+    'semi': 'error',
+    'semi-spacing': 'error',
+    'space-before-blocks': ['error', 'always'],
+    'space-before-function-paren': ['error', {
+      anonymous: 'never',
+      named: 'never',
+      asyncArrow: 'always',
+    }],
+    'space-in-parens': 'error',
+    'space-infix-ops': 'error',
+    'space-unary-ops': 'error',
+    'spaced-comment': ['error', 'always', {
+      'block': { 'balanced': true },
+      'exceptions': ['-'],
+    }],
+    'strict': ['error', 'global'],
+    'symbol-description': 'error',
+    'template-curly-spacing': 'error',
+    'unicode-bom': 'error',
+    'valid-typeof': ['error', { requireStringLiterals: true }],
+
+    // ESLint recommended rules that we disable
+    'no-inner-declarations': 'off',
+  }
+};
